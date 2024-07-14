bbf9ac8c2e43acf3b900952b4cb714a555330c6a
Sendiko <sarangtawon897@gmail.com>
2024-07-14 19:43:52 +0700

    use navhost

--- app/src/main/java/com/sendiko/customnumerickeyboard/MainActivity.kt
+++ app/src/main/java/com/sendiko/customnumerickeyboard/MainActivity.kt
@@ -4,26 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.LargeTopAppBar
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.sendiko.customnumerickeyboard.components.keyboard.NumericKeyboard
-import com.sendiko.customnumerickeyboard.components.selector.Selector
-import com.sendiko.customnumerickeyboard.components.selector.TextFieldMode
-import com.sendiko.customnumerickeyboard.components.textfield.MultipleTextField
-import com.sendiko.customnumerickeyboard.components.textfield.SingleTextField
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.sendiko.customnumerickeyboard.about.AboutScreen
+import com.sendiko.customnumerickeyboard.home.HomeScreen
+import com.sendiko.customnumerickeyboard.home.HomeScreenViewModel
+import com.sendiko.customnumerickeyboard.navigation.AboutScreen
+import com.sendiko.customnumerickeyboard.navigation.HomeScreen
 import com.sendiko.customnumerickeyboard.ui.theme.CustomNumericKeyboardTheme
 
 class MainActivity : ComponentActivity() {
@@ -33,46 +25,21 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CustomNumericKeyboardTheme {
-                val viewModel = viewModel<MainViewModel>()
-                val state by viewModel.state.collectAsState()
-                Scaffold(
-                    modifier = Modifier.fillMaxSize(),
-                    topBar = {
-                        LargeTopAppBar(title = { Text(text = "Custom Numeric Keyboard") })
-                    }
-                ) { innerPadding ->
-                    Column(
-                        modifier = Modifier
-                            .padding(innerPadding)
-                            .fillMaxSize(),
-                        verticalArrangement = Arrangement.Bottom
-                    ) {
-                        AnimatedVisibility(
-                            visible = state.textFieldMode == TextFieldMode.Single,
-                        ) {
-                            SingleTextField(value = state.numberText)
-                        }
-                        AnimatedVisibility(
-                            visible = state.textFieldMode == TextFieldMode.Multiple,
-                        ) {
-                            MultipleTextField(
-                                value = state.numberText,
-                                modifier = Modifier.padding(16.dp),
-                                numberOfTextField = state.numberOfTextField
-                            )
-                        }
-                        Selector(
-                            onSelect = {
-                                viewModel.onEvent(MainEvent.OnChangeMode(it))
-                            },
-                            currentSelected = state.textFieldMode
+                val navController = rememberNavController()
+                NavHost(navController = navController, startDestination = HomeScreen) {
+                    composable<HomeScreen> {
+                        val viewModel = viewModel<HomeScreenViewModel>()
+                        val state by viewModel.state.collectAsState()
+                        HomeScreen(
+                            state = state,
+                            onEvent = viewModel::onEvent,
+                            onNavigate = { navController.navigate(AboutScreen) }
                         )
-                        NumericKeyboard(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .padding(8.dp),
-                            onInput = {
-                                viewModel.onEvent(it)
+                    }
+                    composable<AboutScreen> {
+                        AboutScreen(
+                            onNavigate = {
+                                navController.navigateUp()
                             }
                         )
                     }
